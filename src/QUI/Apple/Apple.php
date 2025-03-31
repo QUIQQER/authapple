995b2e121799da324c5b9432b3791e4c6f18b9bd
Henning <leutz@pcsg.de>
2025-03-31 10:04:18 +0200

    feat: add apple authentication support
    This commit introduces support for Apple-based user authentication. It expands on the existing
    localization, for both German and English languages, to include descriptive context for API
    settings, client and team IDs, and private keys. Corresponding methods have also been added in
    Apple.php to retrieve these details. Besides, the authentication process now includes sign-in with
    Apple by handling the auth token, generating the client secret, and communicating with Apple's
    authentication endpoint. This change should provide users with more options to authenticate
    securely.

--- src/QUI/Apple/Apple.php
+++ src/QUI/Apple/Apple.php
@@ -12,11 +12,38 @@ class Apple
         return QUI::getDBTableName('quiqqer_auth_apple');
     }
 
-    /**
-     * @throws Exception
-     */
     public static function getClientId(): string
     {
         return QUI::getPackage('quiqqer/authapple')->getConfig()->get('apiSettings', 'clientId');
+    }
+
+    /**
+     * Apple Developer Team-ID
+     *
+     * @return string
+     */
+    public static function getTeamId(): string
+    {
+        return QUI::getPackage('quiqqer/authapple')->getConfig()->get('apiSettings', 'teamId');
+    }
+
+    /**
+     * Key-ID from Apple-Key (.p8)
+     *
+     * @return string
+     */
+    public static function getKeyId(): string
+    {
+        return QUI::getPackage('quiqqer/authapple')->getConfig()->get('apiSettings', 'keyId');
+    }
+
+    /**
+     * Private Key (.p8)
+     *
+     * @return string
+     */
+    public static function getPrivateKeyId(): string
+    {
+        return QUI::getPackage('quiqqer/authapple')->getConfig()->get('apiSettings', 'privateKeyId');
     }
 }
--- src/QUI/Apple/Auth.php
+++ src/QUI/Apple/Auth.php
@@ -7,9 +7,11 @@
 namespace QUI\Apple;
 
 use QUI;
+use QUI\Exception;
 use QUI\Interfaces\Users\User;
 use QUI\Users\AbstractAuthenticator;
 use QUI\Locale;
+use Firebase\JWT\JWT;
 
 /**
  * Class Auth
@@ -33,9 +35,53 @@ class Auth extends AbstractAuthenticator
         }
     }
 
-    public function auth(array | int | string $authParams)
+    /**
+     * @throws Exception
+     */
+    public function auth(array | int | string $authParams): void
     {
-        // TODO: Implement auth() method.
+        if (!is_array($authParams) || !isset($authParams['token'])) {
+            throw new QUI\Exception([
+                'quiqqer/authgoogle',
+                'exception.auth.wrong.data'
+            ], 401);
+        }
+
+        $token = $authParams['token'];
+        $code = $authParams['code'];
+
+        $clientId = Apple::getClientId();
+        $teamId = Apple::getTeamId(); // Apple Developer Team-ID
+        $keyId = Apple::getKeyId(); // Key-ID aus deinem Apple-Key (.p8)
+        $privateKey = Apple::getPrivateKeyId(); // Key-ID aus deinem Apple-Key (.p8)
+
+
+        $claims = [
+            'iss' => $teamId,
+            'iat' => time(),
+            'exp' => time() + 86400 * 180,
+            'aud' => 'https://appleid.apple.com',
+            'sub' => $clientId
+        ];
+
+        $clientSecret = JWT::encode($claims, $privateKey, 'ES256', $keyId);
+        $tokenUrl = 'https://appleid.apple.com/auth/token';
+
+        $params = [
+            'grant_type' => 'authorization_code',
+            'code' => $code,
+            'redirect_uri' => '', // @todo
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret
+        ];
+
+        $ch = curl_init($tokenUrl);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $data = json_decode($response, true);
     }
 
     public function getUser(): User
